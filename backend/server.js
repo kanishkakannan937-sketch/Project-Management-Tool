d6fe04e8843942791bf20ce67fbbf9138713af17
Kanishka K <kanishkakannan937@gmail.com>
2026-09-19 13:03:51 +0530

    Update backend server

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected successfully");
 
-        app.listen(PORT, () => {
+        app.listen(PORT,  "0.0.0.0", () => {
             console.log(`Server running on port ${PORT}`);
         });
     })
